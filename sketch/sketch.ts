112285daecd1df1036eb5194166756bbd40f2e08
ikesau <saunders.ike@gmail.com>
2020-08-21 15:21:13 -0400

    feat(eyes): better, more complicated eyes

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,73 +1,182 @@
 let eyes: Eye[] = []
+let ripple: Ripple
 
 class Eye {
   pos: p5.Vector
-  radius: number
-  diameter: number
-  constructor(x: number, y: number, radius: number) {
+  width: number
+  height: number
+  openPercentage: number
+  isOpen: boolean
+
+  constructor(x: number, y: number, width: number) {
     this.pos = createVector(x, y)
-    this.radius = radius
-    this.diameter = radius * 2
+    this.width = width
+    this.height = width * 0.75
+    this.openPercentage = 0
   }
+
+  setIsOpen(isOpen: boolean) {
+    this.isOpen = isOpen
+  }
+
   draw() {
+    if (!this.isOpen) return
     push()
     translate(this.pos.x, this.pos.y)
-    const yOffset = sin((11 / 6) * PI) * this.radius
-    /**
-     * We're moving around the circumference in sixths over/under PI and TWO_PI.
-     * Ignoring the first 4 arguments, arc(PI, TWO_PI) is a semi-circle
-     * This could also be written as arc((6 / 6) * PI, (12 / 6) * PI)
-     * so arc((7 / 6) * PI, (11 / 6) * PI) is an arc that "begins" slightly later
-     * and "ends" slightly earlier
-     *
-     * (11 / 6) * PI can be used as theta in a cartesian transformation
-     * to work out the co-ordinates of the "end" of the arc
-     *
-     * The other 3 co-ordinates are just inverses of this co-ordinate (x,y), (-x,y), (x,-y), (-x,-y)
-     *
-     * By moving the origin of our arcs down by y, we'll make their "starts" and "ends" touch
-     * creating the eyeball shape we're looking for
-     *
-     * It's hard to explain this. Try drawing some points at x,y to see what I mean.
-     */
 
     fill(255)
-    arc(0, -yOffset, this.diameter, this.diameter, (7 / 6) * PI, (11 / 6) * PI)
-    arc(0, yOffset, this.diameter, this.diameter, (13 / 6) * PI, (5 / 6) * PI)
+    beginShape()
+    vertex(-this.width, 0)
+    bezierVertex(
+      -this.width / 2,
+      -this.height,
+      this.width / 2,
+      -this.height,
+      this.width,
+      0
+    )
+    bezierVertex(
+      this.width / 2,
+      this.height,
+      -this.width / 2,
+      this.height,
+      -this.width,
+      0
+    )
+    endShape()
 
     fill(0)
     const pupil = createVector(mouseX, mouseY)
       // translate to origin of eye
       .sub(this.pos)
       // set max magnitude to within the eye
-      .limit(this.radius / 2)
-    circle(pupil.x, pupil.y, this.radius / 1.3)
+      .limit(this.height)
+    circle(pupil.x, pupil.y, this.width * 0.9)
+
+    const maxOpenPercentage = map(pupil.y, -this.height, this.height, 1, 0.6)
+
+    // eyelid
+    if (this.isOpen) {
+      this.openPercentage = lerp(this.openPercentage, maxOpenPercentage, 0.4)
+    }
+    const topLidY = map(
+      this.openPercentage,
+      0,
+      1,
+      this.height * 0.5,
+      -this.height
+    )
+    scale(1, 1.1)
+    beginShape()
+    vertex(-this.width, 0)
+    bezierVertex(
+      -this.width / 2,
+      -this.height,
+      this.width / 2,
+      -this.height,
+      this.width,
+      0
+    )
+    bezierVertex(
+      this.width / 2,
+      topLidY,
+      -this.width / 2,
+      topLidY,
+      -this.width,
+      0
+    )
+    endShape()
+    const bottomLidY = map(
+      this.openPercentage,
+      0,
+      1,
+      this.height * 0.5,
+      this.height
+    )
+    beginShape()
+    vertex(-this.width, 0)
+    bezierVertex(
+      -this.width / 2,
+      bottomLidY,
+      this.width / 2,
+      bottomLidY,
+      this.width,
+      0
+    )
+    bezierVertex(
+      this.width / 2,
+      this.height,
+      -this.width / 2,
+      this.height,
+      -this.width,
+      0
+    )
+    endShape()
 
     pop()
   }
 }
 
-function setup() {
-  createCanvas(windowWidth, windowHeight)
-  noStroke()
-  const rowsAmount = 8
-  const columnsAmount = 8
-  for (let x = 1; x < columnsAmount; x++) {
-    for (let y = 1; y < rowsAmount; y++) {
+function createEyesOutOfNothingAndPutThemIntoABox() {
+  eyes = []
+  background(0)
+
+  const eyeWidth = 40
+  const amount = floor(windowWidth / eyeWidth / 4)
+  let xOff = windowWidth / amount / 2
+  let yOff = windowHeight / amount / 2
+
+  for (let x = 0; x < amount; x++) {
+    for (let y = 0; y < amount; y++) {
       eyes.push(
         new Eye(
-          (x / columnsAmount) * windowWidth,
-          (y / rowsAmount) * windowHeight,
-          20
+          (x * windowWidth) / amount + xOff,
+          (y * windowHeight) / amount + yOff,
+          eyeWidth
         )
       )
     }
   }
 }
 
-function draw() {
-  background(0)
-  if (mouseIsPressed) eyes.forEach((eye) => eye.draw())
+class Ripple {
+  pos: p5.Vector
+  radius: number
+  isComplete: boolean
+  constructor(x: number, y: number) {
+    this.pos = createVector(x, y)
+    this.radius = 1
+    this.isComplete = false
+  }
+  expand() {
+    this.radius += 3
+    eyes.forEach((eye) => {
+      if (p5.Vector.sub(this.pos, eye.pos).mag() < this.radius) {
+        eye.setIsOpen(true)
+      }
+    })
+    if (this.radius > sqrt(sq(windowHeight) + sq(windowWidth))) {
+      this.isComplete = true
+    }
+  }
 }
 
-function windowResized() {}
+function setup() {
+  createCanvas(windowWidth, windowHeight)
+  noStroke()
+  createEyesOutOfNothingAndPutThemIntoABox()
+}
+
+function draw() {
+  eyes.forEach((eye) => eye.draw())
+  if (ripple && !ripple.isComplete) ripple.expand()
+}
+
+function mousePressed() {
+  ripple = new Ripple(mouseX, mouseY)
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight)
+  createEyesOutOfNothingAndPutThemIntoABox()
+}
