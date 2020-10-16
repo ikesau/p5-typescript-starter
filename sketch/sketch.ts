5d00901a1a028ad45a7ab6bf1fbb24844b8f9b44
ikesau <saunders.ike@gmail.com>
2020-10-16 14:56:43 -0400

    feat(game of life): colour evolution

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,9 +1,21 @@
 let grid: Cell[][]
-const CELL_SIZE = 20
+const MUTATION_FACTOR = 40
+let CELL_SIZE = 9
+if (/Mobi|Android/i.test(navigator.userAgent)) {
+  CELL_SIZE = CELL_SIZE * 2
+}
+
 function createGrid(windowWidth: number, windowHeight: number) {
   grid = new Array(floor(windowHeight / CELL_SIZE))
     .fill(undefined)
-    .map((_, yi) => [...new Array(floor(windowWidth / CELL_SIZE)).fill(undefined).map((_, xi) => new Cell(xi, yi))])
+    .map((_, yi) => [
+      ...new Array(floor(windowWidth / CELL_SIZE))
+        .fill(undefined)
+        .map(
+          (_, xi) =>
+            new Cell(xi, yi, undefined, createVector(100 + random(-MUTATION_FACTOR, MUTATION_FACTOR), 100, 100))
+        ),
+    ])
 }
 
 const id = (x: any) => x
@@ -15,14 +27,20 @@ const deepCopy = (x: any): any => {
   return x
 }
 
+function meanVector(vectors: p5.Vector[]) {
+  return vectors.reduce((acc, cur) => p5.Vector.add(acc, cur), new p5.Vector()).div(vectors.length)
+}
+
 class Cell {
   x: number
   y: number
   isAlive: boolean
-  constructor(x: number, y: number, isAlive: boolean = false) {
+  fillColour: p5.Vector
+  constructor(x: number, y: number, isAlive: boolean = Math.random() < 0.5, fillColour: p5.Vector) {
     this.x = x
     this.y = y
     this.isAlive = isAlive
+    this.fillColour = fillColour
   }
   getNeighbours(): Cell[] {
     let neighbours: Cell[] = []
@@ -81,8 +99,27 @@ class Cell {
     return false
   }
 
+  breed(): Cell {
+    const neighbours = this.getNeighbours()
+    const aliveNeighbours = neighbours.filter((cell) => cell.isAlive)
+    if (this.isAlive && (aliveNeighbours.length === 3 || aliveNeighbours.length === 2)) {
+      return this
+    }
+    if (!this.isAlive && aliveNeighbours.length === 3) {
+      const averageFillColour = meanVector(aliveNeighbours.map((cell) => cell.fillColour))
+
+      const mutatedFillColour = averageFillColour.add(
+        createVector(random(-MUTATION_FACTOR, MUTATION_FACTOR), random(-MUTATION_FACTOR, MUTATION_FACTOR))
+      )
+      return new Cell(this.x, this.y, true, mutatedFillColour)
+    }
+    this.isAlive = false
+    return this
+  }
+
   draw() {
     if (this.isAlive) {
+      fill([this.fillColour.x, this.fillColour.y, this.fillColour.z])
       rect(this.x, this.y, 1, 1)
     }
   }
@@ -91,7 +128,6 @@ class Cell {
 function setup() {
   createCanvas(windowWidth, windowHeight)
   createGrid(windowWidth, windowHeight)
-  fill(255, 120, 120)
   rectMode(CENTER)
   noStroke()
   frameRate(12)
@@ -101,9 +137,7 @@ function draw() {
   translate((CELL_SIZE + (windowWidth % CELL_SIZE)) / 2, (CELL_SIZE + (windowHeight % CELL_SIZE)) / 2)
   scale(CELL_SIZE)
 
-  const gridCopy: Cell[][] = deepCopy(grid)
-
-  grid = grid.map((row, yi) => row.map((_, xi) => new Cell(xi, yi, gridCopy[yi][xi].checkWillSurvive())))
+  grid = grid.map((row) => row.map((cell) => cell.breed()))
 
   grid.forEach((row) =>
     row.forEach((cell) => {
@@ -115,9 +149,16 @@ function draw() {
 function addCell() {
   const gridX = floor((mouseX / windowWidth) * floor(windowWidth / CELL_SIZE))
   const gridY = floor((mouseY / windowHeight) * floor(windowHeight / CELL_SIZE))
-  const cell = grid[gridY][gridX]
-  cell.isAlive = true
-  cell.draw()
+  const cell = grid[gridY]?.[gridX]
+  if (cell) {
+    const neighbours = cell.getNeighbours()
+    neighbours.forEach((cell) => {
+      cell.isAlive = true
+      cell.draw()
+    })
+    cell.isAlive = true
+    cell.draw()
+  }
 }
 
 function mouseDragged() {
